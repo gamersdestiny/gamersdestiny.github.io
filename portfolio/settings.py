ba8960f2891edbddfdebcb17e793ac2b5382998d
Guna <gunagang24@gmail.com>
2021-06-18 03:33:54 -0700

    allowed host and req changed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django-heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-i!)a(r+x=+t%7y(l-qjdwye5rld79^*ld-8ctowhuuc&vi%26k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['guna-gd.herokuapp.com']
 
 
 # Application definition
