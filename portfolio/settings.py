9e4ddc8dfb26376b61caf62b19fa0315822f2782
Guna <gunagang24@gmail.com>
2021-06-18 05:03:25 -0700

    css modified and secret key removed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i!)a(r+x=+t%7y(l-qjdwye5rld79^*ld-8ctowhuuc&vi%26k'
+SECRET_KEY = os.environ.get('portfolioSecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
